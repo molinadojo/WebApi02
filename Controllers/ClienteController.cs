0e5f09c237c1ca146a367a5f8ae7dca26178a965
puntacode <pablo.molina@gmail.com>
2024-12-04 13:54:36 -0300

    primer commit Api simple para tener de plantilla

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiSQL.Controllers //de donde sale
+{
+    [ApiController]
+    [Route("cliente")]
+    public class ClienteController : ControllerBase
+    {
+        [HttpGet]
+        [Route("listar")]
+        public dynamic listarCliente()
+        {
+            // todo el codigo que deseemos
+            return new
+            {
+                nombre = "lautaro",
+                apellido = "Molina"
+            };
+        }
+    }
+}
